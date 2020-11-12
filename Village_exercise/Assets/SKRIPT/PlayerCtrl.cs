a589f6434fb3783dc27489211722581323ba223a
Anna Mo <anna.moritz@students.hs-mainz.de>
2020-11-12 12:18:35 +0100

    add controls to car

--- Village_exercise/Assets/SKRIPT/PlayerCtrl.cs
+++ Village_exercise/Assets/SKRIPT/PlayerCtrl.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
+
 {
 
     public float speed = 1.5f; //speed for our red car
     public float verticalInput; // Input of arrow keys up and down
-    
+    public float horizontalInput; // Input of L and R arrow keys 
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +19,10 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         //move our red car
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
     }
 }
