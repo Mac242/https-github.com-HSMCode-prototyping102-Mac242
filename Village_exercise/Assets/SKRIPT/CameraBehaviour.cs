fbc90ebdb9d65858b79d9b7328aabd4c8ebf8f41
Anna Mo <anna.moritz@students.hs-mainz.de>
2020-11-13 11:20:02 +0100

    camera follow car

--- Village_exercise/Assets/SKRIPT/CameraBehaviour.cs
+++ Village_exercise/Assets/SKRIPT/CameraBehaviour.cs
@@ -6,19 +6,22 @@ public class CameraBehaviour : MonoBehaviour
 {
 
 public GameObject car; //red car
+private Transform target;
 
 //offset from local point of car to camera position
-private Vector3 offset = new Vector3(0f,1,-3f); 
+private Vector3 offset = new Vector3(0f,2,-5f); 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = car.transform;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = car.transform.position + offset;
+       // transform.position = car.transform.position + offset;
+        transform.position = target.TransformPoint(offset);
+        transform.LookAt(target);
     }
 }
