e131e1b79aef77148eabc5433525037b83cb4c82
Anna Mo <anna.moritz@students.hs-mainz.de>
2020-11-13 15:15:50 +0100

    add scripts
    tire and tireSpawner

--- Village_exercise/Assets/SKRIPT/PlayerCtrl.cs
+++ Village_exercise/Assets/SKRIPT/PlayerCtrl.cs
@@ -6,8 +6,8 @@ public class PlayerCtrl : MonoBehaviour
 
 {
 
-    public float turnSpeed = 50f; 
-    public float speed = 3f; //speed for our red car
+    public float turnSpeed = 20f; 
+    public float speed = 2f; //speed for our red car
     private float verticalInput; // Input of arrow keys up and down
     private float horizontalInput; // Input of L and R arrow keys 
 
@@ -25,21 +25,16 @@ public class PlayerCtrl : MonoBehaviour
         //move our red car forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
 
-        if(verticalInput > 0)
+        if(verticalInput !=0 )
         {
              //move our red car forward left right
             transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput,Space.Self);
+
             //if I press the arrow keys, my input float is bigger than 0, then do
             // Debug.Log("Vertical Input bigger than null");
         }
-
-        else if(verticalInput < 0)
-        {
-             //move our red car forward left right
-            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput,Space.Self);
-            
-            
-        }
+                
+    
         
     }
 }
